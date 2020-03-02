9283550ab2d989ae53d1574770eb75215bd1f5ae
“SahilKharya” <kharyasahil@gmail.com>
2020-03-02 17:47:30 +0530

    add new customer UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddCustomerComponent } from './add-customer/add-customer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddCustomerComponent,
   ],
   imports: [
     BrowserModule,
@@ -16,3 +18,5 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
