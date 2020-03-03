f417c987e9eb6d9a72352b01230f5b9e3da73a6a
“SahilKharya” <kharyasahil@gmail.com>
2020-03-03 14:06:03 +0530

    new updates

--- src/app/add-customer/add-customer.component.ts
+++ src/app/add-customer/add-customer.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-customer',
   templateUrl: './add-customer.component.html',
   styleUrls: ['./add-customer.component.css']
 })
-export class AddCustomerComponent implements OnInit {
+export class AddCustomerComponent {
 
-  constructor() { }
+  //numericRegex = "^[a-zA-Z0-9]+$";
 
-  ngOnInit(): void {
+  customerForm = this.fb.group({
+    name: ['', Validators.required],
+    mobile: ['', Validators.required]
+  });
+
+  constructor(private fb: FormBuilder) { }
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.customerForm.value);
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,11 +13,10 @@ import { AddCustomerComponent } from './add-customer/add-customer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
-
