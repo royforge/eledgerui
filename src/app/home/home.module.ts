44e76e80e855e2bbc8bc37ee070da58827939676
“SahilKharya” <kharyasahil@gmail.com>
2020-03-23 14:59:01 +0530

    Search in Customers

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,13 +1,11 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { CustomersComponent } from './customers/customers.component';
 import { ReportsComponent } from './reports/reports.component';
-
 
 @NgModule({
   declarations: [HomeComponent, HomepageComponent, CustomersComponent, ReportsComponent],
