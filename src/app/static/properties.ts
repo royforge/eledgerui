6166dbebe4d5f420261cce30f1c03e5ff53965b9
“Arpit <arpitgoyal437@gmail.com>
2020-04-13 12:06:27 +0530

    few changes

--- src/app/static/properties.ts
+++ src/app/static/properties.ts
@@ -5,4 +5,3 @@ export const PAYMENTURL = "http://localhost:8080";
 export const LENDER_URL = "http://localhost:8081/lender";
 export const CUSTOMER_URL = "http://localhost:8081/customer";
 export const UI_URL = "http://localhost:4200"
-                    
